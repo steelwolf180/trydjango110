bca7316693a03165b12408798bd13186392c120d
Max Ong Zong Bao <steelwolf180@gmail.com>
2017-04-25 06:37:49 +0800

    CFE Django 1,10 Lesson 19 of 40

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,31 +1,47 @@
-import random
-import string
 from django.db import models
 
 # Create your models here.
 
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-	# new_code = ''
-	# for _ in range(size):
-	# 	new_code += random.choice(chars)
-	# return new_code
+from .utils import code_generator, create_shortcode
 
-	return ''.join(random.choice(chars) for _ in range(size))
+class KirrURLManager(models.Manager):
+	def all(self, *args, **kwargs):
+		qs_main = super(KirrURLManager,self).all(*args,**kwargs)
+		qs = qs_main.filter(active=True)
+		return qs
+	
+	def refresh_shortcodes(self, items=None):
+		qs = KirrURL.objects.filter(id__gte=1)
+		if items is not None and isinstance(items, int):
+			qs = qs.order_by('-id')[:items]
 
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			print(q.id)
+			q.save()
+			new_codes += 1
+		return "New codes made: {i}".format(i=new_codes)
 
 class KirrURL(models.Model):
 	url 	  = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True, blank=True)
 	updated   = models.DateTimeField(auto_now=True) #every time model is saved set time value
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active 	  = models.BooleanField(default=True)
 	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
 	#shortcode = models.CharField(max_length=15, null=True) empty in database is ok
 	#shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
+	objects = KirrURLManager()
+	#some_random = KirrURLManager() 
 
 	def save(self, *args, **kwargs):
-		print("something")
-		self.shortcode = code_generator()
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode = create_shortcode(self)
 		super(KirrURL, self).save(*args, **kwargs)
+
+	# class Meta:
+	# 	ordering = '-id'
 
 	def my_save(self):
 		self.save()
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def kirr_redirect_view(request, *args, **kwargs): #function based view FBV
+	return HttpResponse("Hello")
+
+class KirrCBView(View):#class based view CBV
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Hello again")
