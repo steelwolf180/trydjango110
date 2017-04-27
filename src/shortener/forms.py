b7c52b5e999605cc28a51fc918a08fbcf671ce53
Max Ong Zong Bao <steelwolf180@gmail.com>
2017-04-27 16:25:18 +0800

    trydjango 1.10 - 32 of 40

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -3,7 +3,16 @@ from django import forms
 from .validators import validate_url, validate_dot_com
 
 class SubmitURLForm(forms.Form):
-	url = forms.CharField(label='Submit URL', validators=[validate_url, validate_dot_com])
+	url = forms.CharField(
+		label='', 
+		validators=[validate_url]
+		widget = form.TextInput(
+				attrs = {
+						"placeholder": "Long URL",
+						"class": "form-control"
+						}
+			)
+		)
 
 	# def clean(self):
 	# 	cleaned_data = super(SubmitURLForm, self,).clean()
