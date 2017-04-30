fc33b32797d538a03717ed43913059ebc9fa3df0
Max Ong Zong Bao <steelwolf180@gmail.com>
2017-04-30 15:47:09 +0800

    add procfile and add .gitignore changes

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -1,45 +1,34 @@
 from django import forms
 
+
 from .validators import validate_url, validate_dot_com
 
-class SubmitURLForm(forms.Form):
-	url = forms.CharField(
-		label='', 
-		validators=[validate_url]
-		widget = form.TextInput(
-				attrs = {
-						"placeholder": "Long URL",
-						"class": "form-control"
-						}
-			)
-		)
+class SubmitUrlForm(forms.Form):
+    url = forms.CharField(
+            label='', 
+            validators=[validate_url],
+            widget = forms.TextInput(
+                    attrs ={
+                        "placeholder": "Long URL",
+                        "class": "form-control"
+                        }
+                )
+            )
 
-	def clean_url(self):
-		url = self.clean.cleaned_data['url']
-		if "http" in url:
-			return url
-		return 'http://' + url
+    # def clean(self):
+    #     cleaned_data = super(SubmitUrlForm, self).clean()
+    #     print(cleaned_data)
+    #     url = cleaned_data.get('url')
+    #     url_validator = URLValidator()
+    #     try:
+    #         url_validator(url)
+    #     except:
+    #         raise forms.ValidationError("Invalid URL for this field")
+    #     return url
+    #     #print(url)
 
-	# def clean(self):
-	# 	cleaned_data = super(SubmitURLForm, self,).clean()
-	# 	print(cleaned_data)
-	# 	url = cleaned_data.get('url')
-	# 	url_validator = URLValidator()
-	# 	try:
-	# 		url_validator(url)
-	# 	except:
-	# 		raise forms.ValidationError("Invalid URL for this field")
-	# 	return url
-	# 	#print(url)
-
-	# def clean_url(self):
-	# 	url = self.cleaned_data['url']
-	# 	#print(url)
-	# 	if not "com" in url:
-	# 		raise forms.ValidationError("This is not valid because no .com")
-		# url_validator = validator()
-		# try:
-		# 	url_validator(url)
-		# except:
-		# 	raise forms.ValidationError("Invalid URL for this field")
-		# return url
+    # def clean_url(self):
+    #     url = self.cleaned_data['url']
+    #     if "http" in url:
+    #         return url
+    #     return "http://" + url
